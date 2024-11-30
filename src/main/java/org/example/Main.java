08981411b7bbdb0d46abf603cc95f76c84305004
atiyemogharnas <atiyemogharnas@gmail.com>
2024-11-30 15:00:39 +0330

    change library to generic and use linkedList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,10 @@ import org.example.systemManagment.Book;
 import org.example.systemManagment.Library;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<Book> books = new ArrayList<>();
         int count = 0;
         Library library = new Library();
 
@@ -29,34 +26,33 @@ public class Main {
                 String year = in.next();
                 System.out.println("please enter a status");
                 String status = in.next();
-                Book book = library.createBook(count++, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
-                books.add(book);
+                library.createBook(count++, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
             } else if (number == 2) {
                 System.out.println("please enter a book id ");
                 Integer id = in.nextInt();
                 System.out.println("please enter a title");
-                String title = in.next();
+                String title = in.nextLine();
                 System.out.println("please enter a author");
-                String author = in.next();
+                String author = in.nextLine();
                 System.out.println("please enter a year");
-                String year = in.next();
+                String year = in.nextLine();
                 System.out.println("please enter a status");
-                String status = in.next();
-                library.updateBook(id, books, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
+                String status = in.nextLine();
+                library.updateBook(id, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
             } else if (number == 3) {
                 System.out.println("please enter a book id ");
                 Integer id = in.nextInt();
-                library.deleteBook(id, books);
+                library.deleteBook(id);
             } else if (number == 4) {
                 System.out.println("please enter a book title");
                 String title = in.next();
-                System.out.println(library.getBooksByTitle(title, books));
+                System.out.println(library.getBooksByTitle(title));
             } else if (number == 5) {
                 System.out.println("please enter a book author");
                 String author = in.next();
-                System.out.println(library.getBooksByAuthor(author, books));
+                System.out.println(library.getBooksByAuthor(author));
             } else if (number == 6) {
-                System.out.println(library.sortedByYearPublication(books));
+                System.out.println(library.sortedByYearPublication());
             }
         } while (number != 0);
 
