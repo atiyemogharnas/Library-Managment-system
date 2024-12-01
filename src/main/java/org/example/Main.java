1fd0b107d787d32755678145412022b12458ea38
atiyemogharnas <atiyemogharnas@gmail.com>
2024-12-01 11:35:25 +0330

    read book from file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,16 @@ package org.example;
 import org.example.systemManagment.Book;
 import org.example.systemManagment.Library;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        int count = 0;
+    public static void main(String[] args) throws IOException {
         Library library = new Library();
 
+        List<Book> books = library.generateBooksFromFile();
 
         Scanner in = new Scanner(System.in);
         System.out.println("لطفا بگویید چه کاری میخواهید انجام دهید: ۱)ایجاد کتاب ۲) ویرایش کتاب ۳) حذف کتاب ۴) جستجو بر اساس عنوان ۵) جستجو بر اساس نویسنده ۶)مرتب کردن کتاب ها بر اساس سال انتشار لطفا عدد مورد نظر را وارد کنید");
@@ -26,7 +28,8 @@ public class Main {
                 String year = in.next();
                 System.out.println("please enter a status");
                 String status = in.next();
-                library.createBook(count++, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
+                library.createBook(books, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
+                System.out.println("کاربر به فایل افزوده شد");
             } else if (number == 2) {
                 System.out.println("please enter a book id ");
                 Integer id = in.nextInt();
@@ -38,21 +41,26 @@ public class Main {
                 String year = in.nextLine();
                 System.out.println("please enter a status");
                 String status = in.nextLine();
-                library.updateBook(id, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
+                library.updateBook(books, id, title, author, LocalDate.parse(year), Book.Status.valueOf(status));
+                System.out.println("کاربر در فایل آبدیت شد");
             } else if (number == 3) {
                 System.out.println("please enter a book id ");
                 Integer id = in.nextInt();
-                library.deleteBook(id);
+                library.deleteBook(books, id);
+                System.out.println("کاربر از فایل حذف شد");
             } else if (number == 4) {
                 System.out.println("please enter a book title");
                 String title = in.next();
-                System.out.println(library.getBooksByTitle(title));
+                library.getBooksByTitle(books, title);
+                System.out.println("فایل بروزرسانی شد");
             } else if (number == 5) {
                 System.out.println("please enter a book author");
                 String author = in.next();
-                System.out.println(library.getBooksByAuthor(author));
+                library.getBooksByAuthor(books, author);
+                System.out.println("فایل بروزرسانی شد");
             } else if (number == 6) {
-                System.out.println(library.sortedByYearPublication());
+                library.sortedByYearPublication(books);
+                System.out.println("فایل بروزرسانی شد");
             }
         } while (number != 0);
 
--- src/main/java/org/example/systemManagment/Library.java
+++ src/main/java/org/example/systemManagment/Library.java
@@ -1,32 +1,64 @@
 package org.example.systemManagment;
 
+import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
 
-public class Library<T extends Book> {
+public class Library {
 
-    LinkedList<T> books = new LinkedList<>();
+    File file = new File("/Users/atiyemogharnas/Documents/Mahsan/book/src/main/resources/bl.txt");
+    int count = 0;
 
-    public void createBook(Integer id, String title, String author, LocalDate yearPublication, Book.Status status) {
-        T book = (T) new Book();
-        book.setId(id);
+    public List<Book> generateBooksFromFile() {
+        List<Book> books = new ArrayList<>();
+        if (!file.exists()) {
+            throw new RuntimeException("file not found");
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Book book = new Book();
+                String[] parts = line.split("_");
+                if (parts.length < 5) continue;
+                book.setId(count++);
+                book.setTitle(parts[1].trim());
+                book.setAuthor(parts[2].trim());
+                book.setYearPublication(LocalDate.parse(parts[3].trim()));
+                book.setStatus(Book.Status.valueOf(parts[4].trim()));
+                books.add(book);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return books;
+    }
+
+    public void createBook(List<Book> books, String title, String author, LocalDate yearPublication, Book.Status status) {
+        if (!books.isEmpty()) {
+            count = books.get(books.size() - 1).getId();
+        }
+        Book book = new Book();
+        book.setId(count++);
         book.setTitle(title);
         book.setAuthor(author);
         book.setYearPublication(yearPublication);
         book.setStatus(status);
         books.add(book);
+        writeInFile(books);
     }
 
-    public void updateBook(Integer id, String title, String author, LocalDate yearPublication, Book.Status status) {
+
+    public void updateBook(List<Book> books, Integer id, String title, String author, LocalDate yearPublication, Book.Status status) {
         if (books.isEmpty()) {
             throw new RuntimeException("non book for update please add book");
         }
         if (id == null) {
             throw new IllegalArgumentException("Book id cannot be null for update");
         }
-        for (Iterator<T> it = books.iterator(); it.hasNext(); ) {
-            T lastBook = it.next();
-            T book = it.next();
+        for (Iterator<Book> it = books.iterator(); it.hasNext(); ) {
+            Book book = it.next();
+            Book lastBook = it.next();
             if (book.getId() == id) {
                 book.setTitle(title == null ? book.getTitle() : title);
                 book.setAuthor(author == null ? book.getAuthor() : author);
@@ -34,29 +66,49 @@ public class Library<T extends Book> {
                 book.setStatus(status == null ? book.getStatus() : status);
                 books.remove(lastBook);
                 books.add(book);
-
             } else {
                 throw new RuntimeException("book id does not match");
             }
+            writeInFile(books);
         }
     }
 
-    public void deleteBook(Integer id) {
+    public void deleteBook(List<Book> books, Integer id) {
         if (id == null) {
             throw new IllegalArgumentException("Book id cannot be null for delete");
         }
         books.removeIf(book -> book.getId() == id);
+        writeInFile(books);
     }
 
-    public List<String> getBooksByTitle(String title) {
-        return books.stream().filter(item -> item.getTitle().equals(title)).map(Book::getAuthor).toList();
+    public void getBooksByTitle(List<Book> books, String title) {
+        books.stream().filter(item -> item.getTitle().equals(title)).toList();
+        writeInFile(books);
+
     }
 
-    public List<String> getBooksByAuthor(String author) {
-        return books.stream().filter(item -> item.getAuthor().equals(author)).map(Book::getTitle).toList();
+    public void getBooksByAuthor(List<Book> books, String author) {
+        books.stream().filter(item -> item.getAuthor().equals(author)).toList();
+        writeInFile(books);
     }
 
-    public List<String> sortedByYearPublication() {
-        return books.stream().sorted(Comparator.comparing(Book::getYearPublication)).map(Book::getTitle).toList();
+    public void sortedByYearPublication(List<Book> books) {
+        books.stream().sorted(Comparator.comparing(Book::getYearPublication)).toList();
+        writeInFile(books);
+    }
+
+    public void writeInFile(List<Book> books) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (Book book : books) {
+                writer.write(String.format("%d_%s_%s_%s_%s\n",
+                        book.getId(),
+                        book.getTitle(),
+                        book.getAuthor(),
+                        book.getYearPublication(),
+                        book.getStatus()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
